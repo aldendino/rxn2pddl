c608217d48e861bf58e24b9688dd278fed2ad128
Alden Ozburn <aldendino@gmail.com>
2015-07-01 13:43:20 -0400

    More modular fast-downward script with timeout and output folder in the configuration.

--- fd_script_all.py
+++ fd_script_all.py
@@ -1,5 +1,6 @@
 import commands
 import sys
+import os.path
 
 # This script takes in a config file with a specific format
 #   The path to the top level directory for fast-downward
@@ -8,6 +9,25 @@ import sys
 #   The path to the text file which lists the problems to be run
 #   The hueristic to be used
 #   A line containing the string "new" or "old", specifying how to run fast-downward
+#   The timeout value (see "man timeout" for options)
+#   The path to the output directory
+
+def run_problem_new(filename, timeout):
+    print(filename)
+    output_file = os.path.join(out_path, filename + ".txt")
+    command = "timeout %s %sfast-downward.py %s %s%s --search %s | tee %s" % (timeout, fd_path, dm_path, pb_path, filename, heuristic, output_file)
+    output = commands.getoutput(command)
+    print(output)
+
+def run_problem_old(filename, timeout):
+    print(filename)
+    output_file = os.path.join(out_path, filename + ".txt")
+    translate_command = "%stranslate/translate.py %s %s%s" % (fd_path, dm_path, pb_path, filename)
+    preprocess_command = "%spreprocess/preprocess < output.sas" % (fd_path)
+    search_command = "%ssearch/downward --search %s < output" % (fd_path, heuristic)
+    command = "timeout %s %s && %s && %s | tee %s" % (timeout, translate_command, preprocess_command, search_command, output_file)
+    output = commands.getoutput(command)
+    print(output)
 
 args = sys.argv[1:]
 
@@ -16,41 +36,33 @@ if len(args) != 1:
 
 config_file = args[0]
 
-(fd_path, dm_path, pb_path, ls_path, heuristic, opt_ver) = map((lambda line: line.rstrip("\n")), open(config_file))
+with open(config_file) as config:
+    (fd_path, dm_path, pb_path, ls_path, heuristic, opt_ver, timeout, out_path) = map((lambda line: line.rstrip("\n")), open(config_file))
 
-print("fast-downard path:     " + fd_path)
-print("domain-path:           " + dm_path)
-print("problems path:         " + pb_path)
-print("problems list path:    " + ls_path)
-print("heuristic:             " + heuristic)
-print("fast-downward version: " + opt_ver)
+    fd_path = os.path.expanduser(fd_path)
+    dm_path = os.path.expanduser(dm_path)
+    pb_path = os.path.expanduser(pb_path)
+    ls_path = os.path.expanduser(ls_path)
+    out_path = os.path.expanduser(out_path)
 
-def run_problem_new(filename):
-    print(filename)
-    output_file = filename + ".txt"
-    command = "timeout 2h %sfast-downward.py %s %s%s --search %s | tee %s" % (fd_path, dm_path, pb_path, filename, heuristic, output_file)
-    output = commands.getoutput(command)
-    print(output)
+    print("fast-downard path:     " + fd_path)
+    print("domain-path:           " + dm_path)
+    print("problems path:         " + pb_path)
+    print("problems list path:    " + ls_path)
+    print("heuristic:             " + heuristic)
+    print("fast-downward version: " + opt_ver)
+    print("timeout:               " + timeout)
+    print("Output path:           " + out_path)
 
-def run_problem_old(filename):
-    print(filename)
-    output_file = filename + ".txt"
-    translate_command = "%stranslate/translate.py %s %s%s" % (fd_path, dm_path, pb_path, filename)
-    preprocess_command = "%spreprocess/preprocess < output.sas" % (fd_path)
-    search_command = "%ssearch/downward --search %s < output" % (fd_path, heuristic)
-    command = "timeout 2h %s && %s && %s | tee %s" % (translate_command, preprocess_command, search_command, output_file)
-    output = commands.getoutput(command)
-    print(output)
+    if opt_ver == "new":
+        func = run_problem_new
+    elif opt_ver == "old":
+        func = run_problem_old
+    else:
+        raise Exception("version option must bve either \"new\" or \"old\"")
 
-if opt_ver == "new":
-    func = run_problem_new
-elif opt_ver == "old":
-    func = run_problem_old
-else:
-    raise Exception("version option must bve either \"new\" or \"old\"")
-
-for line in open(ls_path):
-    filename = line.rstrip("\n")
-    # Ignore blank lines
-    if filename != "":
-        func(filename)
+    for line in open(ls_path):
+        filename = line.rstrip("\n")
+        # Ignore blank lines
+        if filename != "":
+            func(filename, timeout)
